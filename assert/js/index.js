d3e5a5f1952a9ffda885bdf75c3e6ad319c9db59
won0201 <114564396+won0201@users.noreply.github.com>
2023-06-02 17:33:42 +0900

    Add files via upload

---
+++ assert/js/index.js
@@ -0,0 +1,82 @@
+const $form = document.querySelector("form");
+const $input = document.querySelector("input");
+const $comment = document.querySelector("#comment");
+
+// openAI API
+let url = `https://estsoft-openai-api.jejucodingcamp.workers.dev/`;
+
+// 사용자의 질문
+let question;
+
+// 화면에 뿌려줄 데이터, 질문들
+let questionData = [];
+let data = []; // 수정: data 변수 추가
+
+// input에 입력된 질문 받아오는 함수
+$input.addEventListener("input", (e) => {
+  question = e.target.value;
+});
+
+// 사용자의 질문을 객체를 만들어서 push
+const sendQuestion = (question) => {
+  if (question) {
+    data.push({
+      role: "user",
+      content: question,
+    });
+    questionData.push({
+      role: "user",
+      content: question,
+    });
+  }
+};
+
+// 화면에 질문 그려주는 함수
+const printQuestion = async () => {
+  if (question) {
+    let li = document.createElement("li");
+    li.classList.add("question");
+    questionData.map((el) => {
+      li.innerText = el.content;
+    });
+    $chatList.appendChild(li);
+    questionData = [];
+    question = false;
+  }
+};
+
+// 화면에 답변 그려주는 함수
+const printAnswer = (answer) => {
+  let li = document.createElement("li");
+  li.classList.add("answer");
+  li.innerText = answer;
+  $chatList.appendChild(li);
+};
+
+// api 요청보내는 함수
+const apiPost = async () => {
+  const result = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+    redirect: "follow",
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      printAnswer(res.choices[0].message.content);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+// submit
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  $input.value = null;
+  sendQuestion(question);
+  apiPost();
+  printQuestion();
+});
