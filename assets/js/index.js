720b468a66419613a071a6cd8864e6f9604fefaa
won0201 <114564396+won0201@users.noreply.github.com>
2023-06-09 15:07:00 +0900

    로딩 icon 삽입 (js)

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,47 +41,72 @@ const sendQuestion = (question) => {
 };
 
 // 화면에 질문 그려주는 함수
-const printQuestion = () => {
-  if (questionData.length > 0) {
-    let li = document.createElement("li");
-    li.classList.add("question");
+const printQuestion = (question) => {
+  let li = document.createElement("li");
+  li.classList.add("question");
 
-    const questionContainer = document.createElement("div");
-    questionContainer.classList.add("question-container");
+  const questionContainer = document.createElement("div");
+  questionContainer.classList.add("question-container");
 
-    const userIcon = document.createElement("img");
-    userIcon.classList.add("user-icon");
-    userIcon.src = "./assets/img/user.png";
-    userIcon.alt = "User";
+  const userIcon = document.createElement("img");
+  userIcon.classList.add("user-icon");
+  userIcon.src = "./assets/img/user.png";
+  userIcon.alt = "User";
 
-    const questionText = document.createElement("div");
-    questionText.classList.add("question_content");
-    questionText.textContent = questionData[questionData.length - 1].content;
+  const questionText = document.createElement("div");
+  questionText.classList.add("chat_content");
+  questionText.textContent = question;
 
-    questionContainer.appendChild(userIcon);
-    questionContainer.appendChild(questionText);
-    li.appendChild(questionContainer);
-    chatList.appendChild(li);
+  questionContainer.appendChild(userIcon);
+  questionContainer.appendChild(questionText);
+  li.appendChild(questionContainer);
+  chatList.appendChild(li);
+};
+
+// 화면에 로딩 표시를 보여주는 함수
+const showLoadingIndicator = () => {
+  const loadingIndicator = document.createElement("div");
+  loadingIndicator.classList.add("loading-indicator");
+  loadingIndicator.id = "loading-indicator";
+
+  // loadingIndicator를 chatList의 맨 뒤에 추가
+  chatList.appendChild(loadingIndicator);
+};
+
+// 화면에 로딩 표시를 숨기는 함수
+const hideLoadingIndicator = () => {
+  const loadingIndicator = document.getElementById("loading-indicator");
+  if (loadingIndicator) {
+    loadingIndicator.remove();
   }
 };
 
 // API 요청을 보내는 함수
 const apiPost = async () => {
-  const result = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-    redirect: "follow",
-  })
-    .then((res) => res.json())
-    .then((res) => {
-      printAnswer(res.choices[0].message.content);
-    })
-    .catch((err) => {
-      console.log(err);
+  showLoadingIndicator();
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+      redirect: "follow",
     });
+
+    if (response.ok) {
+      const result = await response.json();
+      return result.choices[0].message.content;
+    } else {
+      throw new Error("API 요청에 실패했습니다.");
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    hideLoadingIndicator();
+    data = [];
+  }
 };
 
 // 챗봇의 인사
@@ -113,6 +138,8 @@ const printAnswer = (answer) => {
   answerContainer.appendChild(answerText);
   li.appendChild(answerContainer);
   chatList.appendChild(li);
+
+  hideLoadingIndicator();
 };
 
 // 대화 추가 함수
@@ -127,13 +154,16 @@ function addChat(text, isQuestion) {
 // 채팅 폼 제출 이벤트 핸들러
 document
   .getElementById("chat-form")
-  .addEventListener("submit", function (event) {
+  .addEventListener("submit", async function (event) {
     event.preventDefault();
     const input = document.getElementById("chat-input");
     const message = input.value.trim();
     if (message !== "") {
-      addChat(message, true);
       input.value = "";
+      sendQuestion(message);
+      printQuestion(message);
+      const answer = await apiPost();
+      printAnswer(answer);
     }
   });
 
@@ -143,5 +173,4 @@ chatForm.addEventListener("submit", (e) => {
   chatInput.value = null;
   sendQuestion(question);
   apiPost();
-  printQuestion();
 });
